ccea5dcfc76b52b2cbf788202851df0e38a44ccc
aurelienzeljko <aurelienzeljko@192.168.115.175>
2021-10-18 12:47:04 +0200

    depot ManipNombresInt_TP1

--- TP1_convertisseur_Creseent_Zeljko/src/tp1_convertisseur_creseent_zeljko/TP1_convertisseur_Creseent_Zeljko.java
+++ TP1_convertisseur_Creseent_Zeljko/src/tp1_convertisseur_creseent_zeljko/TP1_convertisseur_Creseent_Zeljko.java
@@ -1,8 +1,10 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * TP1
+ * 18/10
  */
 package tp1_convertisseur_creseent_zeljko;
+
+import java.util.Scanner;
 
 /**
  *
@@ -15,6 +17,86 @@ public class TP1_convertisseur_Creseent_Zeljko {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        double nb1;
+        double nb2;
+        double nb3;
+        double nb4;
+        double nb5;
+        System.out.println(" 1) celcius->Kelvin \n 2) Farenheit-->Celcius \n 3) Celcius-->Farenheit \n 4) Kelvin-->Farenheit \n 5) Farenheit-->Kelvin ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Entrer l'operateur :");
+        int operateur = sc.nextInt();
+
+        if (operateur < 6 && operateur >= 1) {
+
+            if (operateur == 1) {
+                System.out.println("\n Bonjour, saisissez la valeur :");
+                double A = sc.nextDouble();
+                nb1 = CelciusVersKelvin(A);
+                System.out.println("la temperature en kelvin est : " + nb1 + "K");
+            }
+
+            if (operateur == 2) {
+                System.out.println("\n Bonjour, saisissez la valeur :");
+                double B = sc.nextDouble();
+                nb2 = FarenheitVersCelcius(B);
+                System.out.println("la temperature en degres est : " + nb2 + "°");
+            }
+            if (operateur == 3) {
+                System.out.println("\n Bonjour, saisissez la valeur :");
+                double C = sc.nextDouble();
+                nb3 = CelciusVersFarenheit(C);
+                System.out.println("la temperature en farenheit est : " + nb3 + "°F");
+            }
+            if (operateur == 4) {
+                System.out.println("\n Bonjour, saisissez la valeur :");
+                double D = sc.nextDouble();
+                nb4 = KelvinVersFarenheit(D);
+                System.out.println("la temperature en fareheint est : " + nb4 + "°F");
+            }
+            if (operateur == 5) {
+                System.out.println("\n Bonjour, saisissez la valeur :");
+                double E = sc.nextDouble();
+                nb5 = FarenheitVersKelvin(E);
+                System.out.println("la temperature en Kelvin est : " + nb5 + "K");
+            }
+        }
+        else {
+            System.out.println("\n La valeur de l'operateur n'est pas comprise entre 1 et 5");
+        }
     }
+
     
+
+    public static double CelciusVersKelvin(double tempC) {
+        double tempK;
+        tempK = tempC + 273.15;
+        return tempK;
+    }
+
+    public static double FarenheitVersCelcius(double tempF) {
+        double tempC;
+        tempC = (tempF - 32) * (5 / 9.0);
+        return tempC;
+    }
+
+    public static double CelciusVersFarenheit(double tempC) {
+        double tempF;
+        tempF = tempC * (9 / 5.0) + 32;
+        return tempF;
+    }
+
+    public static double KelvinVersFarenheit(double tempK) {
+        double tempF;
+        tempF = tempK * (9 / 5.0) - 459.67;
+        return tempF;
+    }
+
+    public static double FarenheitVersKelvin(double tempF) {
+        double tempK;
+        tempK = (tempF + 459.67) * (5 / 9.0);
+        return tempK;
+    }
+
 }
