eef976d89671e2dc74f23bf51c306bbd41dcd4c4
aurelienzeljko <aurelienzeljko@192.168.115.175>
2021-10-21 16:16:36 +0200

    depot guessmynumber_TP1

--- TP1_guessMyNumber_Cressent_Zeljko/src/tp1_guessmynumber_cressent_zeljko/TP1_guessMyNumber_Cressent_Zeljko.java
+++ TP1_guessMyNumber_Cressent_Zeljko/src/tp1_guessmynumber_cressent_zeljko/TP1_guessMyNumber_Cressent_Zeljko.java
@@ -18,29 +18,70 @@ public class TP1_guessMyNumber_Cressent_Zeljko {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        Random generateurAleat = new Random();
-        int n = generateurAleat.nextInt(100);
+
+        //Random generateurAleat = new Random();
+        //int n = generateurAleat.nextInt(100);
         //System.out.println(n);
+        //Scanner sc = new Scanner(System.in);
+        //System.out.println("\n Entrer un nombre entre 0 et 100 :");
+        //int nb1 = sc.nextInt();
+        //int t = 1;
+        System.out.println(" 1) facile \n 2) moyen \n 3) difficile ");
+        System.out.println("\n Entrer l'operateur :");
         Scanner sc = new Scanner(System.in);
-        System.out.println("\n Entrer un nombre entre 0 et 100 :");
-        int nb1 = sc.nextInt();
-        while (nb1 != n) {
+        int operateur = sc.nextInt();
+        if (operateur == 1) {
+            Random generateurAleat = new Random();
+            int n = generateurAleat.nextInt(100);
+            //System.out.println(n);
+            System.out.println("\n Entrer un nombre entre 0 et 100 :");
+            int nb1 = sc.nextInt();
+            int t = 1;
+            while (nb1 != n) {
+                t = t + 1;
                 if (nb1 == n) {
                     System.out.println("\n gagné");
-                    break;
                 }
                 if (nb1 < n) {
                     System.out.println("\n trop petit");
-                    
+
                 }
                 if (nb1 > n) {
                     System.out.println("\n trop grand");
-                }   
+                }
                 System.out.println("\n Entrer un nombre entre 0 et 100 :");
                 nb1 = sc.nextInt();
+                System.out.println("gagné");
+
+            }
+            System.out.println("vous avez gagné en :" + t + "essai");
+        }
+        if (operateur == 2) {
+            Random generateurAleat = new Random();
+            int n = generateurAleat.nextInt(100);
+            //System.out.println(n);
+            System.out.println("\n Entrer un nombre entre 0 et 100 :");
+            int nb1 = sc.nextInt();
+            int t = 1;
+            while (nb1 != n) {
+                t = t + 1;
+                if (nb1 == n) {
+                    System.out.println("\n gagné");
+                }
+                if (nb1 < n) {
+                    System.out.println("\n trop petit");
+
+                }
+                if (nb1 > n) {
+                    System.out.println("\n trop grand");
+                }
+                System.out.println("\n Entrer un nombre entre 0 et 100 :");
+                nb1 = sc.nextInt();
+                System.out.println("gagné");
+
+            }
+            System.out.println("vous avez gagné en :" + t + "essai");
 
         }
     }
-
 }
