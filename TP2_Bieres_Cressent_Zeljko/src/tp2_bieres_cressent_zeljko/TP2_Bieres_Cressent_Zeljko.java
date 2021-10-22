dda8e0fca66e9183a84bd9e4345fa070413b1a62
aurelienzeljko <aurelienzeljko@192.168.115.175>
2021-10-22 11:47:42 +0200

    deuxieme depot TP2_Bieres_Cressent_Zeljko, presque fini

--- TP2_Bieres_Cressent_Zeljko/src/tp2_bieres_cressent_zeljko/TP2_Bieres_Cressent_Zeljko.java
+++ TP2_Bieres_Cressent_Zeljko/src/tp2_bieres_cressent_zeljko/TP2_Bieres_Cressent_Zeljko.java
@@ -16,16 +16,25 @@ public class TP2_Bieres_Cressent_Zeljko {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        bouteilleBiere uneBiere = new bouteilleBiere("Cuvée des trolls", 7.0 ,"Dubuisson") ;
-        uneBiere.nom = "Cuvée des trolls";
-        uneBiere.degreAlcool = 7.0 ;
-        uneBiere.brasserie="Dubuisson";
+        bouteilleBiere uneBiere = new bouteilleBiere("Cuvée des trolls", 7.0 ,"Dubuisson",true) ;
+        bouteilleBiere uneBiere1 = new bouteilleBiere("Tripel Karmeliet", 8.4 ,"Bosteels",false) ;
+        bouteilleBiere uneBiere2 = new bouteilleBiere("Paix Dieu", 10.0 ,"Caulier",false) ;
+        bouteilleBiere uneBiere3 = new bouteilleBiere("Chouffe", 8.0 ,"Achouffe",true) ;
+        //uneBiere.nom = "Cuvée des trolls";
+        //uneBiere.degreAlcool = 7.0 ;
+        //uneBiere.brasserie="Dubuisson";
+        //uneBiere.lireEtiquette();
+        //uneBiere.nom = "Leffe";
+        //uneBiere.degreAlcool = 6.6 ;
+        //uneBiere.brasserie = "Abbaye de Leffe";
         uneBiere.lireEtiquette();
-        uneBiere.nom = "Leffe";
-        uneBiere.degreAlcool = 6.6 ;
-        uneBiere.brasserie = "Abbaye de Leffe";
-        uneBiere.lireEtiquette();
-        
+        uneBiere1.lireEtiquette();
+        uneBiere2.lireEtiquette();
+        uneBiere3.lireEtiquette();
+        uneBiere.Décapsuler();
+        uneBiere1.Décapsuler();
+        //uneBiere2.Décapsuler();
+        //uneBiere3.Décapsuler();
     }
     
 }
--- TP2_Bieres_Cressent_Zeljko/src/tp2_bieres_cressent_zeljko/bouteilleBiere.java
+++ TP2_Bieres_Cressent_Zeljko/src/tp2_bieres_cressent_zeljko/bouteilleBiere.java
@@ -17,12 +17,27 @@ public class bouteilleBiere {
     
 
     public void lireEtiquette() {
-        System.out.println("Bouteille de " + nom +" (" + degreAlcool + " degres) \nBrasserie : " + brasserie ) ;
+        System.out.println("Bouteille de " + nom +" (" + degreAlcool + " degres) \nBrasserie : " + brasserie  ) ;
     }
-    public bouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+    public bouteilleBiere(String unNom, double unDegre, String uneBrasserie, boolean ouverte) {
         nom = unNom;
         degreAlcool = unDegre;
         brasserie = uneBrasserie;
         ouverte = false;
+    }
+    public void Décapsuler() {
+        if (ouverte=false) {
+            ouverte=true;
+            System.out.println("je viens de l'ouvrir, SANTE !");
+        }
+        else{
+            System.out.println("ta biere est deja ouverte, SANTE !");
+        }
+    }
+    public String toString() {
+        String chaine_a_retourner;
+        chaine_a_retourner = nom + " (" + degreAlcool + " degrés)Ouverte ? ";
+        if (ouverte == true ) chaine_a_retourner += "oui" ; else chaine_a_retourner += "non" ;
+        return chaine_a_retourner ;
     }
 }
