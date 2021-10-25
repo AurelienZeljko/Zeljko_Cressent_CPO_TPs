f0dca05f12ba7f1b55a542ed2b83182ad84f4dc2
aurelienzeljko <aurelienzeljko@192.168.115.175>
2021-10-25 11:20:23 +0200

    debut Exo 3 TP2

---
+++ TP2_manip_Cressent_Zeljko/src/tp2_manip_cressent_zeljko/TP2_manip_Cressent_Zeljko.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_manip_cressent_zeljko;
+
+/**
+ *
+ * @author aurelienzeljko
+ */
+public class TP2_manip_Cressent_Zeljko {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Tartiflette assiette1 = new Tartiflette(500) ;
+        Tartiflette assiette2 = new Tartiflette(600) ;
+        assiette1.Tartiflette();
+    }
+    
+}
---
+++ TP2_manip_Cressent_Zeljko/src/tp2_manip_cressent_zeljko/Tartiflette.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_cressent_zeljko;
+
+/**
+ *
+ * @author aurelienzeljko
+ */
+public class Tartiflette {
+    int nbCalories;
+    
+    public static int Tartiflette (int nb) {
+        int KAL = nb;
+    return KAL;
+    }
+}
