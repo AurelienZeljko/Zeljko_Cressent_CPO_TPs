ebd78283d88c930ec4893681085e6e22a9202c77
aurelienzeljko <aurelienzeljko@192.168.115.175>
2021-10-25 16:31:12 +0200

    Super Puissance 4. class Joueur fini

---
+++ SP4_console_Cressent _Zeljko/src/sp4_console_cressent/_zeljko/Joueur.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_cressent._zeljko;
+
+/**
+ *
+ * @author aurelienzeljko
+ */
+public class Joueur {
+    String nom;
+    String couleur;
+    Jeton[]ListeJetons;
+    int nombreDesintegrateurs;
+    int nombreJetonsRestants;
+    
+    public  Joueur(String s) {
+        nom = s;
+        nombreDesintegrateurs = 0;
+        nombreJetonsRestants = 0;
+
+    }
+    
+    public void affecterCouleur(String couleur1){
+        couleur=couleur1;
+    }
+    
+    public boolean ajouterJeton (Jeton unjeton) {
+        for ( int i=0; i<21 ; i++){
+            if (ListeJetons[i]==null){
+                ListeJetons[i]=unjeton;
+                nombreJetonsRestants++;
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
+    public void obtenirDesintegrateur(){
+        nombreDesintegrateurs++;
+    }
+    
+    public boolean utiliserDesintegrateur (){
+        if (nombreDesintegrateurs!=0){
+            nombreDesintegrateurs--;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+
