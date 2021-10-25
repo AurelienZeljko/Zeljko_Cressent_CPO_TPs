234f45e9d69c3de30e22c36f6e3d09f55d87a197
aurelienzeljko <aurelienzeljko@192.168.115.175>
2021-10-25 10:41:36 +0200

    tp2 non fini mais bug a regler

--- TP2_convertisseurObjet_Cressent_Zeljko/src/tp2_convertisseurobjet_cressent_zeljko/Convertisseur.java
+++ TP2_convertisseurObjet_Cressent_Zeljko/src/tp2_convertisseurobjet_cressent_zeljko/Convertisseur.java
@@ -9,6 +9,44 @@ package tp2_convertisseurobjet_cressent_zeljko;
  * @author aurelienzeljko
  */
 public class Convertisseur {
-    int nbConversions ;
-    
+
+    int nbConversions;
+
+    public static double CelciusVersKelvin(double tempC) {
+        double tempK;
+        tempK = tempC + 273.15;
+        return tempK;
+    }
+
+    public double FarenheitVersCelcius(double tempF) {
+        double tempC;
+        tempC = (tempF - 32) * (5 / 9.0);
+        return tempC;
+    }
+
+    public double CelciusVersFarenheit(double tempC) {
+        double tempF;
+        tempF = tempC * (9 / 5.0) + 32;
+        return tempF;
+    }
+
+    public double KelvinVersFarenheit(double tempK) {
+        double tempF;
+        tempF = tempK * (9 / 5.0) - 459.67;
+        return tempF;
+    }
+
+    public double FarenheitVersKelvin(double tempF) {
+        double tempK;
+        tempK = (tempF + 459.67) * (5 / 9.0);
+        return tempK;
+    }
+
+    public Convertisseur() {
+        nbConversions = 0;
+    }
+    @Override
+    public String toString () {
+        return "nb de conversions"+ nbConversions;
+    }
 }
--- TP2_convertisseurObjet_Cressent_Zeljko/src/tp2_convertisseurobjet_cressent_zeljko/TP2_convertisseurObjet_Cressent_Zeljko.java
+++ TP2_convertisseurObjet_Cressent_Zeljko/src/tp2_convertisseurobjet_cressent_zeljko/TP2_convertisseurObjet_Cressent_Zeljko.java
@@ -15,7 +15,9 @@ public class TP2_convertisseurObjet_Cressent_Zeljko {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        Convertisseur Temp1 = new Convertisseur();
+        Temp1.CelciusVersFarenheit(15);
+        System.out.println(Temp1);
+        Temp1.toString();
     }
-    
 }
