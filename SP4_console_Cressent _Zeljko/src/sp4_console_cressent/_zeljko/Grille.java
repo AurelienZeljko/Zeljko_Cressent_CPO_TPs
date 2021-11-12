98c11f779608476f584d621c96982949758c6e61
aurelienzeljko <aurelienzeljko@192.168.115.175>
2021-11-12 11:09:18 +0100

    creation grille debut

---
+++ SP4_console_Cressent _Zeljko/src/sp4_console_cressent/_zeljko/Grille.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_cressent._zeljko;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aurelienzeljko
+ */
+public class Grille {
+    Cellule [][] CellulesJeu = new Cellule[6][7];  
+    
+    public Grille(){
+        for ( int i=0 ; i<=6 ; i++){
+            for ( int j=0 ; i<=7 ; i++){
+                CellulesJeu[i][j]=new Cellule();
+        }
+    }
+    }
+    public boolean ajouterJetonDansColonne(Jeton,int){
+        
+    }
+}
