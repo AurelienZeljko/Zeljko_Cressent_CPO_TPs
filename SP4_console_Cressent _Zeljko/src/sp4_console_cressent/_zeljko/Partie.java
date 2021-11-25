d419107d91a34e4c49d0dcf9b13a945393292844
aurelienzeljko <aurelienzeljko@192.168.115.175>
2021-11-25 15:03:34 +0100

    probleme partie

--- SP4_console_Cressent _Zeljko/src/sp4_console_cressent/_zeljko/Partie.java
+++ SP4_console_Cressent _Zeljko/src/sp4_console_cressent/_zeljko/Partie.java
@@ -24,41 +24,93 @@ public class Partie {
 
     }
 
-    public void attribuerCouleursAuxJoueurs() {
-        ListeJoueurs[0].affecterCouleur("rouge");
-        ListeJoueurs[1].affecterCouleur("jaune");
-    }
-
-    public void initialiserPartie() {
-        grilleJeu = new Grille();
-        for (int i = 0; i < 21; i++) {
-            Jeton JetonJoueur1 = new Jeton("rouge");
-            ListeJoueurs[0].ajouterJeton(JetonJoueur1);
-            Jeton JetonJoueur2 = new Jeton("jaune");
-            ListeJoueurs[1].ajouterJeton(JetonJoueur2);
-        }
-        Random nombre = new Random();
-        for (int i = 1; i < 6; i++) {
-            boolean trouNoir = false;
-            while (trouNoir == false) {
-                int A = nombre.nextInt(6);
-                int B = nombre.nextInt(7);
-                trouNoir = grilleJeu.placerTrouNoir(A, B);
-            }
-        }
-
-        
-    }
 
     public void debuterPartie() {
         grilleJeu.afficherGrilleSurConsole();
-        Scanner sc= new Scanner(System.in); 
-        int colonne=0;
+        Scanner sc = new Scanner(System.in);
+        int colonne = 0;
         System.out.println("Choisissez une colonne entre 1 et 7");
-        colonne=sc.nextInt();
-        Jeton J= new Jeton();
-        if (colonne<=7 && colonne>=1){
+        colonne = sc.nextInt();
+        Jeton J = new Jeton("Rouge");
+        if (colonne <= 7 && colonne >= 1) {
             grilleJeu.ajouterJetonDansColonne(J, colonne);
+        }
+    }
+
+    public void initialiserPartie() {
+//        grilleJeu.viderGrille();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nom du premier joueur :");
+        Joueur joueur1 = new Joueur(sc.nextLine());
+        System.out.println("nom du deuxieme joueur :");
+        Joueur joueur2 = new Joueur(sc.nextLine());
+        ListeJoueurs[0] = joueur1;
+        ListeJoueurs[1] = joueur2;
+
+        attribuerCouleurAuxJoueurs();
+        Random couleur = new Random();
+        int aleat = couleur.nextInt(2);
+        if (aleat == 1) {
+            joueurCourant = ListeJoueurs[0];
+            System.out.println(ListeJoueurs[0].nom + " commence.");
+        } else {
+            joueurCourant = ListeJoueurs[1];
+            System.out.println(ListeJoueurs[1].nom + " commence.");
+        }
+        for (int i = 0; i < 21; i++) {
+            Jeton unJeton = new Jeton(ListeJoueurs[0].couleur);
+            ListeJoueurs[0].ajouterJeton(unJeton);
+            Jeton unJeton2 = new Jeton(ListeJoueurs[1].couleur);
+            ListeJoueurs[1].ajouterJeton(unJeton2);
+        }
+        Random nombre = new Random();
+        for (int i = 1; i < 3; i++) {
+            boolean trouNoir = false;
+            boolean Desintegrateur = false;
+            while (trouNoir == false && Desintegrateur == false) {
+                int A = nombre.nextInt(6);
+                int B = nombre.nextInt(7);
+                trouNoir = grilleJeu.placerTrouNoir(A, B);
+                Desintegrateur = grilleJeu.placerDesintegrateur(A, B);
+            }
+        }
+        Random nombre1 = new Random();
+        for ( int i=1 ; i<4 ; i++){
+            boolean trouNoir = false;
+            while(trouNoir == false ){
+                int A = nombre1.nextInt(6);
+                int B = nombre1.nextInt(7);
+                trouNoir = grilleJeu.placerTrouNoir(A, B);
+            }
+        }
+        Random nombre3 = new Random();
+        for ( int i=1 ; i<4 ; i++){
+            boolean Desintegrateur = false;
+            while(Desintegrateur == false ){
+                int A = nombre3.nextInt(6);
+                int B = nombre3.nextInt(7);
+                Desintegrateur = grilleJeu.placerDesintegrateur(A, B);
+            }
+        }
+            
+        
+        grilleJeu.afficherGrilleSurConsole();
+
+    }
+
+    public void attribuerCouleurAuxJoueurs() {
+        Random couleur = new Random();
+        int nbr = couleur.nextInt(2);
+        if (nbr == 1) {
+            ListeJoueurs[0].affecterCouleur("Jaune");
+            System.out.println(ListeJoueurs[0].nom + " est Jaune.");
+            ListeJoueurs[1].affecterCouleur("Rouge");
+            System.out.println(ListeJoueurs[1].nom + " est Rouge.");
+        } else {
+            ListeJoueurs[0].affecterCouleur("Rouge");
+            System.out.println(ListeJoueurs[0].nom + " est Rouge.");
+            ListeJoueurs[1].affecterCouleur("Jaune");
+            System.out.println(ListeJoueurs[1].nom + " est Jaune.");
         }
     }
 }
--- SP4_console_Cressent _Zeljko/src/sp4_console_cressent/_zeljko/SP4_console_Cressent_Zeljko.java
+++ SP4_console_Cressent _Zeljko/src/sp4_console_cressent/_zeljko/SP4_console_Cressent_Zeljko.java
@@ -15,21 +15,14 @@ public class SP4_console_Cressent_Zeljko {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Jeton J = new Jeton("Rouge");
-        System.out.println(J.lireCouleur());
-        Cellule c=new Cellule();
-        System.out.println(c.lireCouleurDuJeton());
-        c.affecterJeton(J);
-        System.out.println(c.lireCouleurDuJeton());
-        Grille g=new Grille();
-        g.ajouterJetonDansColonne(J, 2);
+
         String A="aurel";
-        String B="chloe";
+        String B="jules";
         Joueur Joueur1 = new Joueur(A);
         Joueur Joueur2 = new Joueur(B);
         Partie unePartie = new Partie(Joueur1,Joueur2);
         unePartie.initialiserPartie();
-        unePartie.debuterPartie();
+        //unePartie.debuterPartie();
     }
     
 }
